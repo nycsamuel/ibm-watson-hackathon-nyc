defc743cba4f4674e46315d3266479898841b49e
Sang Min Na <xsangmin@gmail.com>
2017-01-21 19:22:04 -0500

    watson

---
+++ app.js
@@ -0,0 +1,306 @@
+/*eslint-env node, express*/
+
+// This application uses express as its web server
+// for more info, see: http://expressjs.com
+var express = require("express");
+var request = require("request");
+var crypto = require("crypto");
+var fetch = require('node-fetch');
+
+var APP_ID = process.env.APP_ID;
+var APP_SECRET = process.env.APP_SECRET;
+var WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
+
+const WWS_URL = "https://api.watsonwork.ibm.com";
+const AUTHORIZATION_API = "/oauth/token";
+var WEBHOOK_VERIFICATION_TOKEN_HEADER = "X-OUTBOUND-TOKEN".toLowerCase();
+
+// create a new express server
+var app = express();
+
+// serve the files out of ./public as our main files
+app.use(express.static(__dirname + "/public"));
+
+function rawBody(req, res, next) {
+    var buffers = [];
+    req.on("data", function(chunk) {
+        buffers.push(chunk);
+    });
+    req.on("end", function() {
+        req.rawBody = Buffer.concat(buffers);
+        next();
+    });
+}
+
+function errorHandler(err, req, res, next) {
+    if (res.headersSent) {
+        return next(err);
+    }
+    res.status(500);
+    res.render("error", {
+        error: err
+    });
+}
+
+app.use(rawBody);
+app.use(errorHandler);
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("INFO: app is listening on port: " + (process.env.PORT || 3000));
+});
+
+app.post("/webhook_callback", function(req, res) {
+
+
+  if (!APP_ID || !APP_SECRET || !WEBHOOK_SECRET) {
+  	console.log("ERROR: Missing variables APP_ID, APP_SECRET or WEBHOOK_SECRET from environment");
+  	return;
+  }
+
+  if (!verifySender(req.headers, req.rawBody)) {
+      console.log("ERROR: Cannot verify caller! -------------");
+      console.log(req.rawBody.toString());
+      res.status(200).end();
+      return;
+  }
+
+  var body = JSON.parse(req.rawBody.toString());
+  var eventType = body.type;
+  if (eventType === "verification") {
+      handleVerificationRequest(res, body.challenge);
+      console.log("INFO: Verification request processed");
+      return;
+  }
+
+  // Acknowledge we received and processed notification to avoid getting sent the same event again
+  res.status(200).end();
+
+
+  if (eventType !== "message-annotation-added") {
+    console.log("INFO: Skipping unwanted eventType: " + eventType);
+    return;
+  }
+
+  if (body.userId === APP_ID) {
+    console.log("INFO: Skipping our own message Body: " + JSON.stringify(body));
+    return;
+  }
+
+
+  const spaceId = body.spaceId;
+
+  var msgTitle = "Watson says:";
+  var msgText = "You sound ";
+  var memberName = "";
+  var memberId = "";
+  var msgQuote = '';
+  var msgGiphy = '';
+
+  const annotationType = body.annotationType;
+  var messageId = body.messageId;
+  var annotationPayload = JSON.parse(body.annotationPayload);
+
+  // console.log('*****', annotationPayload);
+
+  // if (annotationType === 'message-nlp-keywords') {
+  //   console.log('**** INSIDE ****')
+    
+  //   var keywords = annotationPayload.keywords;
+  //   var searchTerms = keywords.map(function(obj) {
+  //     return obj.text;
+  //   })
+    
+  //   console.log('*** search terms *** ', searchTerms);
+  //   var queryTerms = searchTerms.join('+');
+  //   var GIPHY_API_URL = `http://api.giphy.com/v1/gifs/search?q=${queryTerms}&api_key=dc6zaTOxFJmzC`;
+    
+  //   // request(GIPHY_API_URL)
+  //   // .then(r => r.json())
+  //   // .then(data => {
+  //   //   console.log('*** data ***', data)
+  //   // })
+  //   // .catch(err => console.log(err));
+
+  //   msgTitle = 'Giphy Time';
+  //   msgText = ' giphy giphy giphy';
+
+
+
+  //   console.log('**** EXITING ****')
+  // }
+
+  if (annotationType === "message-nlp-docSentiment") {
+    var docSentiment = annotationPayload.docSentiment;
+
+    var GIPHY_URL = 'http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC';
+    var QUOTES_URL = 'http://quotesondesign.com/api/3.0/api-3.0.json';
+
+    fetch(GIPHY_URL)
+    .then(r => r.json())
+    .then(data => {
+      // console.log('*** data ***', data.data);
+      var giphyURL = data.data.image_original_url;
+      msgGiphy = giphyURL;
+
+      if (docSentiment.type === "negative" && docSentiment.score < -0.50) {
+        msgText = "sad. Here's a quote to cheer you up: ";
+      } else if (docSentiment.type === "positive" && docSentiment.score > 0.50) {
+        msgText = "happy. Here's a quote to keep you happy: ";
+      } else {
+        // If the person is neither happy nor sad then assume neutral and just return
+        msgText = "neither happy nor sad. Here's a quote to improve your mood: ";
+      }
+
+      
+
+
+    })
+    .then(doFetch => {
+      fetch('http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json')
+      .then(r => r.json())
+      .then(data => {
+        console.log('**** data ****', data)
+        console.log(data.quoteText)
+        msgQuote = data.quoteText;
+      })
+    })
+    .catch(err => console.log(err));
+
+  } else {
+      // Skip analysis we are not interested in
+      return;
+  }
+
+  // Build request options for authentication.
+  const authenticationOptions = {
+    "method": "POST",
+    "url": `${WWS_URL}${AUTHORIZATION_API}`,
+    "auth": {
+        "user": APP_ID,
+        "pass": APP_SECRET
+    },
+    "form": {
+        "grant_type": "client_credentials"
+    }
+  };
+
+  request(authenticationOptions, function(err, response, authenticationBody) {
+
+    // If successful authentication, a 200 response code is returned
+    if (response.statusCode !== 200) {
+        // if our app can't authenticate then it must have been disabled.  Just return
+        console.log("ERROR: App can't authenticate");
+        return;
+    }
+    const accessToken = JSON.parse(authenticationBody).access_token;
+
+    const GraphQLOptions = {
+        "url": `${WWS_URL}/graphql`,
+        "headers": {
+            "Content-Type": "application/graphql",
+            "x-graphql-view": "PUBLIC",
+            "jwt": "${jwt}"
+        },
+        "method": "POST",
+        "body": ""
+    };
+
+    GraphQLOptions.headers.jwt = accessToken;
+    GraphQLOptions.body = "{ message (id: \"" + messageId + "\") {createdBy { displayName id}}}";
+
+    request(GraphQLOptions, function(err, response, graphqlbody) {
+
+      if (!err && response.statusCode === 200) {
+          const bodyParsed = JSON.parse(graphqlbody);
+          var person = bodyParsed.data.message.createdBy;
+					memberId = person.id;
+          memberName = person.displayName;
+          // msgText = memberName + msgText;
+          msgText = `${msgText} ${msgQuote} - and here's a funny gif for you. ${msgGiphy}`;
+      } else {
+          console.log("ERROR: Can't retrieve " + GraphQLOptions.body + " status:" + response.statusCode);
+          return;
+      }
+
+      // Avoid endless loop of analysis :-)
+			if (memberId !== APP_ID) {
+        const appMessage = {
+            "type": "appMessage",
+            "version": "1",
+            "annotations": [{
+                "type": "generic",
+                "version": "1",
+
+                "title": "",
+                "text": "",
+                "color": "#ececec",
+            }]
+        };
+
+        const sendMessageOptions = {
+            "url": "https://api.watsonwork.ibm.com/v1/spaces/${space_id}/messages",
+            "headers": {
+                "Content-Type": "application/json",
+                "jwt": ""
+            },
+            "method": "POST",
+            "body": ""
+        };
+
+        sendMessageOptions.url = sendMessageOptions.url.replace("${space_id}", spaceId);
+        sendMessageOptions.headers.jwt = accessToken;
+        appMessage.annotations[0].title = msgTitle;
+        appMessage.annotations[0].text = msgText;
+        sendMessageOptions.body = JSON.stringify(appMessage);
+
+        request(sendMessageOptions, function(err, response, sendMessageBody) {
+
+          if (err || response.statusCode !== 201) {
+              console.log("ERROR: Posting to " + sendMessageOptions.url + "resulted on http status code: " + response.statusCode + " and error " + err);
+          }
+
+        });
+      }
+      else {
+        console.log("INFO: Skipping sending a message of analysis of our own message " + JSON.stringify(body));
+      }
+    });
+  });
+});
+
+
+
+function verifySender(headers, rawbody) {
+    var headerToken = headers[WEBHOOK_VERIFICATION_TOKEN_HEADER];
+    var endpointSecret = WEBHOOK_SECRET;
+    var expectedToken = crypto
+        .createHmac("sha256", endpointSecret)
+        .update(rawbody)
+        .digest("hex");
+
+    if (expectedToken === headerToken) {
+        return Boolean(true);
+    } else {
+        return Boolean(false);
+    }
+}
+
+function handleVerificationRequest(response, challenge) {
+    var responseBodyObject = {
+        "response": challenge
+    };
+    var responseBodyString = JSON.stringify(responseBodyObject);
+    var endpointSecret = WEBHOOK_SECRET;
+
+    var responseToken = crypto
+        .createHmac("sha256", endpointSecret)
+        .update(responseBodyString)
+        .digest("hex");
+
+    response.writeHead(200, {
+        "Content-Type": "application/json; charset=utf-8",
+        "X-OUTBOUND-TOKEN": responseToken
+    });
+
+    response.end(responseBodyString);
+}
